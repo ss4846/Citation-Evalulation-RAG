de15ba5dc2c9b2b44fcbed87c04e20d3819af7da
ss4846 <ss4846@srmist.edu.in>
2026-08-13 19:40:42 +0100

    initial setup: project structure, dependencies, requirements

---
+++ check_install.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+import faiss
+import sentence_transformers
+import mistralai
+import ragas
+import langchain
+import pandas
+import scipy
+import sklearn
+import matplotlib
+
+print('faiss:', faiss.__version__)
+print('sentence-transformers:', sentence_transformers.__version__)
+print('ragas:', ragas.__version__)
+print('langchain:', langchain.__version__)
+print('pandas:', pandas.__version__)
+print('MISTRAL_API_KEY loaded:', bool(os.getenv("MISTRAL_API_KEY")))
+print('All good - ready to build.')
